b064cfae0559cc9864b21529289dffda0d1519ce
Tripuravaram Chandrasekhar Reddy <62606087+chandra1729ch@users.noreply.github.com>
2023-11-26 10:49:51 +0530

    crud operations

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Content from './Content';
 import Footer from './Footer';
 import { useState, useEffect } from 'react';
 import SearchItem from './SearchItem';
+import apiRequest from './apiRequest';
 
 function App() {
-  const API_URL = "http://localhost:3500/items";
+const API_URL = "http://localhost:3500/items";
 const [items,setItems] =  useState([]);
 const [fetchErrors, setFetchErrors] = useState(null);
 const [isLoading, setIsLoading] = useState(true);
-const handleCheck = (id) => {
+const handleCheck = async (id) => {
   const listItems = items.map((item) => id === item.id ? {...item, checked: !item.checked} : item);
   setItems(listItems);
+  const myItem = listItems.filter(item => item.id === id);
+  const updateOptions = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({checked: myItem[0].checked})
+  }
+  const reqUrl = `${API_URL}/${id}`
+  const result = await apiRequest(reqUrl, updateOptions);
+  if(result) setFetchErrors(result);
 }
 useEffect(() => {
   const fetchItems = async() => {
@@ -33,9 +45,16 @@ useEffect(() => {
   },2000)
 }, [])
 
-const handleDelete = (id) => {
+const handleDelete = async (id) => {
   const itemsFilter = items.filter(item => item.id !== id);
   setItems(itemsFilter);
+
+  const deleteOptions =  {
+    method: 'DELETE'
+  }
+  const reqUrl = `${API_URL}/${id}`
+  const result = await apiRequest(reqUrl, deleteOptions);
+  if(result) setFetchErrors(result);
 }
 const [search, setSearch] = useState('');
 const [newItem, setNewItem] = useState('');
@@ -45,11 +64,20 @@ const handleSubmit = (e) => {
   addItem(newItem);
   setNewItem("");
 }
-const addItem = (item) => {
+const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id+1 : 1;
     const myNewItem = {id, checked: false, item};
     const listItems = [...items, myNewItem];
     setItems(listItems);
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+    const result = await apiRequest(API_URL, postOptions)
+    if(result) setFetchErrors(result);
 }
   return (
     <div className="App">
---
+++ src/apiRequest.js
@@ -0,0 +1,12 @@
+const apiRequest = async (url = '', optionsObj = null, errorMsg = null) => {
+    try {
+        const response = await fetch(url, optionsObj);
+        if(!response.ok) throw Error("Please reload the Application......")
+    } catch (error) {
+        errorMsg = error.message;
+    } finally {
+        return errorMsg;
+    }
+}
+
+export default apiRequest;
